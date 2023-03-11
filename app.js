2ada46d3142470f3b8d923238c599f4df3649f7f
ahmed <ahmedzaidi2002@gmail.com>
2023-03-11 11:40:41 +0530

    modifead

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 export default app;
@@ -22,11 +22,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
 
-    cookie: {
-      secure: process.env.NODE_ENV === "development" ? false : true,
-      httpOnly: process.env.NODE_ENV === "development" ? false : true,
-      sameSite: process.env.NODE_ENV === "development" ? false : "none",
-    }
+    // cookie: {
+    //   secure: process.env.NODE_ENV === "development" ? false : true,
+    //   httpOnly: process.env.NODE_ENV === "development" ? false : true,
+    //   sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    // }
   })
 );
 
@@ -37,11 +37,11 @@ app.use(urlencoded({
   extended: true,
 }));
 
-app.use(cors({
-  credentials: true,
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}))
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.FRONTEND_URL,
+//   methods: ["GET", "POST", "PUT", "DELETE"]
+// }))
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
